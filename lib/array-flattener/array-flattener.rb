81ddbd475cf189dc294e7638f8d0af9b9fd4a092
Declan Butler <declan.butler@gmail.com>
2017-10-15 19:38:59 +0100

    recursive and iterative methods

--- lib/array-flattener/array-flattener.rb
+++ lib/array-flattener/array-flattener.rb
@@ -1,31 +1,48 @@
-def flatten(arr)
-
-  if arr.nil?
-    return false
-  end
-
-  return [arr] unless arr.is_a? Array
-
-  arr.inject([]) {|result, element| #explain
-    result.concat(flatten(element))}
-end
-
-def check_only_numeric(arr)
-  # check if elements in array are numeric
-  if arr.any?
-    return true
-  end
-end
-
-def flatten2(array)
-  result = []
-  array.each do |element|
-    unless element.is_a? Array
-      result << element
+def flatten_iteratively (input_array)
+  return nil if input_array.nil?
+  flatList = []
+  while input_array.length > 0
+    item = input_array.shift
+    if item.is_a? Array
+      # add the nested array back so elements can be added
+      input_array = item + input_array
     else
-      flatten2(element).each {|element| result << element}
+      flatList << check_if_int(item)
     end
   end
+  return flatList
+end
 
-  result
+def flatten_using_each(input_array)
+  return nil if input_array.nil?
+  flattened_array = []
+  input_array.each_with_index do |element, index|
+    # puts "#{index}: #{element}"
+    unless element.is_a? Array
+      # p flattened_array
+      flattened_array << check_if_int(element)
+    else
+      flatten_using_each(element).each {|element| flattened_array << element}
+    end
+  end
+  flattened_array
+end
+
+# def flatten_using_reduce(input_array)
+#   return nil if input_array.nil?
+#   input_array.reduce([]) do |result, element|
+#     unless element.is_a? Array
+#       result << check_if_int(element)
+#     else
+#       result + flatten_using_reduce(element)
+#     end
+#   end
+# end
+
+def check_if_int(element)
+  if element.is_a? Integer
+    return element
+  else
+    raise ArgumentError.new("Only integers are allowed")
+  end
 end
--- tests/test_array-flattener.rb
+++ tests/test_array-flattener.rb
@@ -5,37 +5,47 @@ require 'benchmark'
 class TestArrayFlattener < Test::Unit::TestCase
 
   def test_flatten
-    test_array = [1, 2, 3, [4, 5], [[6]], 7, [[[8, 9], 10], 11], [], [[[[[[[[[[[[[[[[[[[[[1000]]]]]]]]]]]]]]]]]]]]]]
+
+    nested_array = [-2, -1, 0, [2, [3], 1, 1, []], 4, [[5]], [], [6, [7, 7], 7]]
+
     puts Benchmark.measure {
-      50_000.times {
-        assert_equal(test_array.flatten, flatten(test_array))
+      100.times {
+        assert_equal(nested_array.flatten, flatten_using_each(nested_array))
       }
     }
+
     puts Benchmark.measure {
-      50_000.times {
-        assert_equal(test_array.flatten, flatten2(test_array))
+      100.times {
+        assert_equal(nested_array.flatten, flatten_iteratively(nested_array))
       }
     }
+
   end
 
   def test_flatten_with_char
-    test_array_with_char = [1, 2, 3, [4, 5], [[6]], 'a']
-    assert_equal(test_array_with_char.flatten, flatten(test_array_with_char))
+    nested_array_with_char = [-2, -1, 0, [1, 2, [3]], 4, [[5, 'a']], [], [6]]
+    assert_raise ArgumentError do
+      flatten_iteratively(nested_array_with_char)
+    end
+    assert_raise ArgumentError do
+      flatten_using_each(nested_array_with_char)
+    end
   end
 
-  def test_flatten_empty
+  def test_flatten_with_empty_array
     empty_array = []
-    assert_equal(empty_array.flatten, flatten(empty_array))
+    assert_equal(empty_array.flatten, flatten_iteratively(empty_array))
+    assert_equal(empty_array.flatten, flatten_using_each(empty_array))
   end
 
-  def test_flatten_null
-    nil_var = nil
-    assert_equal(false, flatten(nil_var))
+  def test_flatten_with_nil
+    assert_nil(flatten_iteratively(nil))
+    assert_nil(flatten_using_each(nil))
   end
 
-  def test_check_only_numeric
-    test_array_with_char = [1, 2, 3, [4, 5], [[6]], 'a']
-    assert_equal(true, check_only_numeric(test_array_with_char))
+  def test_check_if_int_raises_error
+    assert_raise ArgumentError do
+      check_if_int('a')
+    end
   end
-
 end
